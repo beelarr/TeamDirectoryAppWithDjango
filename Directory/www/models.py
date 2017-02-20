fc650a39dd00a53a4a32edfe32beeabb4f6421c4
beelarr <bryonl@me.com>
2017-02-20 12:01:01 -0600

    Added slug auto and details template/urls

--- Directory/www/models.py
+++ Directory/www/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
 class Person (models.Model):
@@ -6,6 +8,14 @@ class Person (models.Model):
     email = models.CharField(max_length=60)
     title = models.CharField(max_length=60)
     image = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=40, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.name)
+        super(Person, self).save(*args, **kwargs)
+
+
 
     def __str__(self):
         return self.name
